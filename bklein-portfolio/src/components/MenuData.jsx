8635ab40af6a5ff5d94958f2ec65b919875c7059
Brian Klein <bklein1981@gmail.com>
2023-10-22 20:07:25 -0500

    added navigation bar

---
+++ bklein-portfolio/src/components/MenuData.jsx
@@ -0,0 +1,26 @@
+export const MenuData = [
+    {
+        title: 'About Me',
+        url: '/',
+        class: 'nav-links',
+        icon: 'fa-solid fa-mug-saucer'
+    },
+    {
+        title: 'Portfolio',
+        url: '/',
+        class: 'nav-links',
+        icon: 'fa-solid fa-code'
+    },
+    {
+        title: 'Contact',
+        url: '/',
+        class: 'nav-links',
+        icon: 'fa-solid fa-network-wired'
+    },
+    {
+        title: 'Curriculum Vitae',
+        url: '/',
+        class: 'nav-links',
+        icon: 'fa-solid fa-file-code'
+    }
+]
